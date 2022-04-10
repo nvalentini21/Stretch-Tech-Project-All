5e03dc5eb91840677083046c82de5ae02384b9a1
Nicole Valentini <ncvalentini@gmail.com>
2022-04-10 15:28:55 -0600

    Add basic server set up

--- stretch-tech-api/server.js
+++ stretch-tech-api/server.js
@@ -1,1 +1,9 @@
-//Test commit
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.set('port', process.env.PORT || 3000)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
